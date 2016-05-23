73feff74d234b84a4838d300b2cfdc0f9a08f5ec
Alexandr Grigorev <agrigorev@cmtknet.ru>
2016-05-23 13:25:24 +0300

    DisplayMessageActivity added

--- app/src/main/java/ru/stc/droidclient/MainActivity.java
+++ app/src/main/java/ru/stc/droidclient/MainActivity.java
@@ -8,8 +8,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
         public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
-        ...
-    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +21,6 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
